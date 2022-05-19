a4820b5771e5679f26d5ae5f8dd531dfd4af25c2
MattE55 <89932886+MattE55@users.noreply.github.com>
2022-05-18 21:08:50 -0700

    5.5
    Some functions are still not working but working on functionality of sending images and location via chat

--- README.md
+++ README.md
@@ -6,9 +6,38 @@
 
 I created this app with careerfoundry to learn how to build a mobile chat app using react native. The app will provide users with a chat interface and options to share messages, images, and locations.
 
+## Key Features
+
+- User can create a username on the start page which, upon enering chat, will welcome them and show their name by their messages.
+- Users can select their chat background color.
+- Users can send text, saved pictures, new pictures, and current location
+- online chat is stored via firebase
+- Saved chats can be viewed offline
+
 ## Technologies Used
 
 - React Native
 - Expo
 - Google Firestore Database
+- Gifted Chat
 - JavaScript
+
+## Installation
+
+### Prerequisites
+
+- Node.js and npm
+- Android Studio or Xcode for iOS simulators
+- Expo / Expo Go
+
+### Install packages from package.json
+
+- Download this repo
+- Navigate to root folder
+- Install required packages in package.json
+
+### Run
+
+- navigate to root folder
+- run 'expo start' or 'npm start' from the terminal
+- Use one of the simulators listed before to open on a mobile simulator
--- components/Chat.js
+++ components/Chat.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text, Platform, KeyboardAvoidingView } from 'react-native';
 import { GiftedChat, Bubble, InputToolbar} from 'react-native-gifted-chat';
 import * as firebase from 'firebase';
 import "firebase/firestore";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import NetInfo from '@react-native-community/netinfo';
+import * as Permissions from 'expo-permissions';
+import * as ImagePicker from 'expo-image-picker';
+import CustomActions from './CustomActions';
+import MapView from 'react-native-maps';
 
 
 export default class Chat extends React.Component {
@@ -18,7 +22,9 @@ export default class Chat extends React.Component {
         name: "",
         avatar: "",
       },
-      isConnected: false
+      isConnected: false,
+      image: null,
+      location: null,
     }
 
   //information for the database
@@ -55,7 +61,8 @@ export default class Chat extends React.Component {
   // Save messages to async storage
   async saveMessages() {
     try {
-      await AsyncStorage.setItem('messages', JSON.stringify(this.state.messages));
+      await AsyncStorage.setItem('messages', 
+        JSON.stringify(this.state.messages));
     } catch (error) {
       console.log(error.message);
     }
@@ -74,9 +81,12 @@ export default class Chat extends React.Component {
   }
 
   componentDidMount() {
+    // Preloaded message to greet the developer
+    // Second message saying the user has entered the chat by username
     let name = this.props.route.params.name;
     this.props.navigation.setOptions({ title: name });
 
+    // checks users connection
     NetInfo.fetch().then(connection => {
       if (connection.isConnected) {
         console.log('online');
@@ -138,13 +148,25 @@ export default class Chat extends React.Component {
       // gets the QueryDocumentSnapshot's data
       let data = doc.data();
       messages.push({
-        _id: data._id, 
+        _id: data._id,
         createdAt: data.createdAt.toDate(),
         text: data.text,
-        user: data.user,
+        user: {
+          _id: data.user._id,
+          name: data.user.name,
+          avatar: data.user.avatar,
+        },
+        image: data.image || null,
+        location: data.location || null,
       });
-   });
-};
+    });
+    // set the messages 
+    this.setState({
+      messages: messages,
+    });
+    // save the messages
+    this.saveMessages();
+  };
 
   addMessage() {
     const message = this.state.messages[0];
@@ -153,7 +175,9 @@ export default class Chat extends React.Component {
       _id: message._id,
       createdAt: message.createdAt,
       text: message.text,
-      user: this.state.user
+      user: this.state.user,
+      image: message.image || null,
+      location: message.location || null,
     });
   }
 
@@ -198,7 +222,34 @@ export default class Chat extends React.Component {
     }
   }
 
+  renderCustomView (props) {
+    const { currentMessage } = props;
+    if (currentMessage.location) {
+      return (
+          <MapView
+            style={{width: 150,
+              height: 100,
+              borderRadius: 13,
+              margin: 3}}
+            region={{
+              latitude: currentMessage.location.latitude,
+              longitude: currentMessage.location.longitude,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+          />
+      );
+    }
+    return null;
+  }
+
+  // displays the communication features
+  renderCustomActions = (props) => {
+    return <CustomActions {...props} />;
+  };
+
   render() {
+    //user selected background color from the start page
     const { Color } = this.props.route.params;
 
     return (
@@ -212,14 +263,16 @@ export default class Chat extends React.Component {
           messages={this.state.messages}
           onSend={messages => this.onSend(messages)}
           renderInputToolbar={this.renderInputToolbar.bind(this)}
+          renderActions={this.renderCustomActions}
+          renderCustomView={this.renderCustomView}
           user={{
             _id: this.state.user._id,
             name: this.state.name,
-            avatar: this.state.user.avatar
+            avatar: this.state.user.avatar,
         }}
         />
         {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
-      </View>
+      </View> 
     )
   }
 }
